415254a77d1ce829ad5b1d38d9feda5530eb2dcd
Mohd Suffian Mohd Shah <sufisha91@gmail.com>
2015-11-14 04:57:22 +0800

    view
    edit css, dashboard

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,4 @@
-<footer class="main-footer text-center">
-                <div class="pull-center hidden-xs ">
-                    <b>Version</b> 2.0
-                </div>
-                <strong>Copyright &copy; 2014-2015 <a href="http://www.iat.com.my">IIUM Advanced Technology Sdn. Bhd.</a>.</strong> All rights reserved.
+<footer class="main-footer text-center" style="line-height:30px">
+                <div class="pull-center hidden-xs">              
+                <strong>Copyright &copy; 2014-2015 <a href="http://www.iat.com.my">IIUM Advanced Technology Sdn. Bhd.</a>.</strong> All rights reserved.</div>
 </footer>
